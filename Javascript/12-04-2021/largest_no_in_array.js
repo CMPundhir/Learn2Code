d837e75a3306c437adce1f44406a9f16d46fc0b7
CMPundhir <g.pundhir8802805015@gmail.com>
2021-04-14 15:55:20 +0530

    objects and array

--- Javascript/12-04-2021/largest_no_in_array.js
+++ Javascript/12-04-2021/largest_no_in_array.js
@@ -3,8 +3,9 @@ console.log("Hello node");
 let arr = [2, 3, 6, 6, 5];
 console.log(arr);
 
-let largestNo = -762783263862863;
-let secondLargestNo = largestNo;
+let largestNo = 0;
+let secondLargestNo = 0;
+
 for (var i = arr.length - 1; i >= 0; i--) {
 	if(largestNo < arr[i]){
 		secondLargestNo = largestNo;
---
+++ Javascript/14-04-2021/object_ex.js
@@ -0,0 +1,37 @@
+var name = "CM"
+
+console.log(name)
+
+console.log(typeof name)
+
+var car1 = {
+	color: "black",
+	name: "jaguar",
+	price: 5000000,
+	startEngine: function () {
+		console.log("brrruuuuuuuhhhhhhh... engine running...")
+	}
+}
+
+console.log(car1)
+
+console.log(car1.color)
+
+
+var car2 = {
+	color: 'white',
+	name: 'nano',
+	price: 150000,
+	startEngine: function () {
+		console.log(`${this.name} is starting....`)
+		console.log("tuk tuk...")
+	}
+}
+
+console.log(car2)
+
+console.log(typeof car2)
+
+car1.startEngine()
+car2.startEngine()
+
