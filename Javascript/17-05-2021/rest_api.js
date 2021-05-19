af8206c0c384f7598382d1d7cef87e6622b25c28
CMPundhir <g.pundhir8802805015@gmail.com>
2021-05-19 15:14:21 +0530

    post login api

---
+++ Javascript/17-05-2021/rest_api.js
@@ -0,0 +1,5 @@
+// Rest API -> REspresntational State Transfer API
+// API -> Application Program Interface
+
+// REST is a set of architectural constraints, not a protocol or a standard. API developers can implement REST in a variety of ways.
+
